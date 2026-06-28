7de660e1f4a0fc76aee6d5d56b272268c0b53999
K12-K <kalp12122000@gmail.com>
2026-06-28 14:27:11 +0530

    updated cors urls in collector

--- apps/collector/src/app.ts
+++ apps/collector/src/app.ts
@@ -11,7 +11,7 @@ export const app = Fastify({
 
 // ✅ ADD THIS
 app.register(cors, {
-  origin: ["http://localhost:5173","http://localhost:5174","https://dashboard-analytics-plat.vercel.app"],
+  origin: ["http://localhost:5173","http://localhost:5174","https://dashboard-analytics-plat.vercel.app","https://tracker-test-prod.up.railway.app"],
   credentials: true,
 });
 
